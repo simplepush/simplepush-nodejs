27b7b56b1d53a004d2af77522935928ce695ff17
Timm <tymmm1@gmail.com>
2022-01-16 21:23:49 +0100

    Add actions

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const https = require('https')
 const crypto = require('crypto')
 
-const options = {
-    hostname: 'api.simplepush.io',
-    port: 443,
-    path: '/send',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-}
+const url = 'api.simplepush.io'
 
-exports.send = function({key, title, message, event, password, salt}, err) {
+exports.send = function({key, title, message, event, actions, password, salt}, err, feedbackCallback = null, feedbackCallbackTimeout = 60) {
     if (key == null || message == null) {
-        return err(new Error("Key and message cannot be empty"))
+        return err(new SimplepushError("Key and message cannot be empty"))
+    }
+
+    if (Array.isArray(actions) && actions.length > 0) {
+        if (typeof actions[0] === 'string') {
+            if (!actions.every(a => (typeof a === "string"))) {
+                return err(new SimplepushError("Feedback actions malformed"))
+            }
+        } else {
+            if (!actions.every(a => a.hasOwnProperty('name') && a.hasOwnProperty('url'))) {
+                return err(new SimplepushError("Get actions malformed"))
+            }
+        }
     }
 
     var data
@@ -23,7 +27,8 @@ exports.send = function({key, title, message, event, password, salt}, err) {
             key: key,
             title: title,
             msg: message,
-            event: event
+            event: event,
+            actions: actions
         })
     } else if (typeof password === 'string' && typeof salt === 'string') {
         const IV_LENGTH = 16
@@ -55,16 +60,35 @@ exports.send = function({key, title, message, event, password, salt}, err) {
             title: encryptedTitle,
             msg: encryptedMessage,
             event: event,
+            actions: actions,
             encrypted: 'true',
             iv: ivBytes.toString('hex')
         })
     } else {
-        return err(new Error("Make sure to set both password and salt if you want to send an encrypted message"))
+        return err(new SimplepushError("Make sure to set both password and salt if you want to send an encrypted message"))
+    }
+
+    const options = {
+        hostname: url,
+        port: 443,
+        path: '/send',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
     }
 
     const req = https.request(options, res => {
-        res.on('close', function() {
-            err()
+        var body = ''
+        res.on('data', function(chunk) {
+            body += chunk
+        })
+
+        res.on('end', function() {
+            const response = JSON.parse(body)
+            if (response.feedbackId) {
+                queryFeedbackEndpoint(response.feedbackId, feedbackCallbackTimeout, feedbackCallback, err)
+            }
         })
     })
 
@@ -75,3 +99,79 @@ exports.send = function({key, title, message, event, password, salt}, err) {
     req.write(data)
     req.end()
 }
+
+async function queryFeedbackEndpoint(feedbackId, timeout, callback, err) {
+    var stop = false
+    var n = 0
+    var start = Date.now() / 1000 | 0
+    const options = {
+        hostname: url,
+        port: 443,
+        path: '/1/feedback/' + feedbackId,
+        method: 'GET'
+    }
+
+    while (!stop) {
+        const req = https.request(options, res => {
+            var body = ''
+            res.on('data', function(chunk) {
+                body += chunk
+            })
+
+            res.on('end', function() {
+                const r = JSON.parse(body)
+                if (r.success && r.action_selected) {
+                    stop = true
+                    return callback({
+                            actionSelected: r.action_selected,
+                            actionSelectedAt: r.action_selected_at,
+                            actionDeliveredAt: r.action_delivered_at,
+                            feedbackId: feedbackId
+                        })
+                } else if (!r.success) {
+                    stop = true
+                    return err(new SimplepushError(`Feedback ${feedbackId} expired or doesn't exist`))
+                }
+            })
+        })
+
+        req.on('error', error => {
+            err(error)
+        })
+
+        req.end()
+
+        if (timeout) {
+            var now = Date.now() / 1000 | 0
+            if (now > start + timeout) {
+                stop = true
+                return err(new SimplepushError("Action callback timeout for feedback " + feedbackId))
+            }
+        }
+
+        if (n < 60) {
+            // In the first minute query every second
+            await wait(1000)
+        } else if (n < 260) {
+            // In the ten minutes after the first minute query every 3 seconds
+            await wait(3000)
+        } else {
+            // After 11 minutes query every five seconds
+            await wait(5000)
+        }
+    }
+}
+
+function wait(time) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
+}
+
+function SimplepushError(message = "") {
+    this.name = "SimplepushError";
+    this.message = message;
+}
+SimplepushError.prototype = Error.prototype;
